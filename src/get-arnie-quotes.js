9ee033c978565439c88c9344f01b2c1bf6b4d59e
Ambar Bose <ambarbs@users.noreply.github.com>
2025-09-22 11:08:43 +1000

    Implement getArnieQuotes function

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,16 @@
 const { httpGet } = require('./mock-http-interface');
 
-const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+/**
+ * Fetch quotes from URLs and normalize results:
+ * If 200 then:  { "Arnie Quote": message }, else: { "FAILURE": message }
+ */
+const getArnieQuotes = async (urls = []) => {
+  const results = await Promise.all(urls.map(httpGet));
+  return results.map(({ status, body }) => {
+    // body is a JSON string like '{"message":"..."}'
+    const { message } = JSON.parse(body);
+    return { [status === 200 ? 'Arnie Quote' : 'FAILURE']: message };
+  });
 };
 
 module.exports = {
